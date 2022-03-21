37206d02ef0dac1db19afabdb19794eef6bb62c4
Tina Manaktala <45269738+tinamanaktala@users.noreply.github.com>
2022-03-21 10:46:52 +0530

    adding different error messages for different exceptions

--- ccprocess/src/main/java/com/restapp/ccprocess/exception/CustomExceptionHandler.java
+++ ccprocess/src/main/java/com/restapp/ccprocess/exception/CustomExceptionHandler.java
@@ -12,11 +12,18 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 	
-	@ExceptionHandler({MissingFieldsException.class,InvalidCardException.class})
-	public final ResponseEntity<Object> handleMissingFieldsException(Exception ex, WebRequest request) throws Exception {
+	@ExceptionHandler(InvalidCardException.class)
+	public final ResponseEntity<Object> handleInvalidCardException(Exception ex, WebRequest request) throws Exception {
 		ExceptionResponse exceptionResponse =	new ExceptionResponse(new Date(), "Invalid card data",ex.getMessage());
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
 	}
+	
+	@ExceptionHandler(MissingFieldsException.class)
+	public final ResponseEntity<Object> handleMissingFieldsException(Exception ex, WebRequest request) throws Exception {
+		ExceptionResponse exceptionResponse =	new ExceptionResponse(new Date(), "Missing mandatory fields",ex.getMessage());
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+	
 	
 	
 
